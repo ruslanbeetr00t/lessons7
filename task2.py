47d15558f73a485a2bc1931eb90f0c2336274372
пк <treshmetal.ua@gmail.com>
2021-06-01 18:52:18 +0300

    init

---
+++ task2.py
@@ -0,0 +1,12 @@
+"""
+Creating a dictionary. Create a function called make_country, which takes in a country’s name and capital as
+parameters. Then create a dictionary from those two, with ‘name’ as a key and ‘capital’ as a parameter. Make the
+function print out the values of the dictionary to make sure that it works as intended.
+"""
+
+
+def make_countyr(**kwargs):
+    print(kwargs)
+
+
+make_countyr(name='Ukraine', capital='Kyiv')
